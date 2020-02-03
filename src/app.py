1094b0295dc75dc4cfcb6773565a7803778aaf51
William Ma <whoiswillma@gmail.com>
2020-02-02 21:46:49 -0500

    Add response

--- src/app.py
+++ src/app.py
@@ -3,5 +3,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/event/', methods=['POST'])
+def receive_slack_event():
+    body = json.loads(request.data)
+    if body['type'] == 'url_verificaton':
+        return body['challenge']
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
