97c0c378b1d95ed50978761f78d767e9dbaabbd8
William Ma <whoiswillma@gmail.com>
2020-02-03 18:40:29 +0000

    Finish v1

--- src/app.py
+++ src/app.py
@@ -1,13 +1,97 @@
 import json
-from flask import Flask, request
+import requests
+import os
+import random
+import sys
 
-app = Flask(__name__)
+bearer_token = 'Bearer ' + os.getenv('API_KEY')
 
-@app.route('/event/', methods=['POST'])
-def receive_slack_event():
-    body = json.loads(request.data)
-    if body['type'] == 'url_verificaton':
-        return body['challenge']
+def slack_pr(endpoint, data): 
+    return requests.post(
+        endpoint,
+        data = json.dumps(data),
+        headers = { 
+            'Authorization': bearer_token,
+            'Content-type': 'application/json'
+        }
+    ).json()
+
+def slack_gr(endpoint, params):
+    return requests.get(
+        endpoint,
+        params = params,
+        headers = { 
+            'Authorization': bearer_token,
+            'Content-type': 'application/x-www-form-urlencoded'
+        }
+    ).json()
+
+def slack_get_bagelers():
+    res = slack_gr('https://slack.com/api/users.conversations', None) 
+    bagelers = set()
+    for channel in res['channels']:
+        channel_id = channel['id']
+
+        members = slack_gr('https://slack.com/api/conversations.members',
+                           { 'channel': channel_id })['members']
+        
+        for user in members:
+            bagelers.add(user)
+    
+    return list(bagelers)
+
+def slack_create_group(users):
+    res = slack_pr('https://slack.com/api/conversations.open', { 'users': users })
+    print(res)
+    channel_id = res['channel']['id']
+
+    res = slack_pr('https://slack.com/api/chat.postMessage', { 
+        'text': 'Hello, is this thing on?',
+        'channel': channel_id
+    })
+    print(res)
+    res = slack_pr('https://slack.com/api/chat.postMessage', { 
+        'text': 'Well anyways, enjoy your first three person coffee chat!',
+        'channel': channel_id
+    })
+    print(res)
+
+def divvy_up(members, group_size):
+    mbrs = members.copy()
+
+    smallGroups = group_size - (len(members) % group_size)
+    largeGroups = (len(members) - (group_size - 1) * smallGroups) // group_size
+    groups = []
+
+    for i in range(0, smallGroups):
+        group = []
+        for j in range(0, group_size - 1):
+            group.append(mbrs.pop()) 
+
+        groups.append(group)
+
+    for i in range(0, largeGroups):
+        group = []
+        for j in range(0, group_size):
+            group.append(mbrs.pop())
+
+        groups.append(group)
+
+    return groups
+     
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    bagelers = slack_get_bagelers()
+    random.shuffle(bagelers)
+    groups = divvy_up(bagelers, 4)
+
+    for arg in sys.argv[1:]:
+        if arg == 'print': 
+            for group in groups:
+                print(group)
+        if arg == 'make':
+            for group in groups:
+                slack_create_group(group)
+
+            
+
