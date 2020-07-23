fda342ec9caa30b2b09fc030c798d252e1aebe6f
William Ma <whoiswillma@gmail.com>
2020-07-23 19:48:22 -0400

    Fix verification code

--- slack_event.go
+++ slack_event.go
@@ -6,6 +6,7 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -13,23 +14,13 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	bytes, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	err = r.Body.Close()
+	requestBody, err := verifyRequest(r)
 	if err != nil {
 		panic(err)
 	}
 
 	var jsonBody map[string]interface{}
-	err = json.Unmarshal(bytes, &jsonBody)
-	if err != nil {
-		panic(err)
-	}
-
-	err = verifyRequest(r)
+	err = json.Unmarshal(requestBody, &jsonBody)
 	if err != nil {
 		panic(err)
 	}
@@ -48,63 +39,62 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 var didWarnUnverifiedRequests = false
 
-func verifyRequest(r *http.Request) error {
+func verifyRequest(r *http.Request) ([]byte, error) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return nil, err
+	}
+
 	if SlackSigningSecret == "" {
-		if didWarnUnverifiedRequests {
-			return nil
+		if !didWarnUnverifiedRequests {
+			log.Warning("The BAGEL_SLACK_SIGNING_SECRET environment variable is empty; requests will not be verified")
+			didWarnUnverifiedRequests = true
 		}
 
-		log.Warning("The BAGEL_SLACK_SIGNING_SECRET environment variable is empty; requests will not be verified")
-		didWarnUnverifiedRequests = true
+		return requestBody, nil
 	}
 
 	timestamp := r.Header.Get("X-Slack-Request-Timestamp")
-	requestBody, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return err
-	}
+
+	fmt.Println(string(requestBody))
 
 	timestampInt, err := strconv.ParseInt(timestamp, 10, 64)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	now := time.Now().Unix()
 	if timestampInt > now {
-		return errors.New("request has a timestamp in the future")
+		return nil, errors.New("request has a timestamp in the future")
 	}
 
 	var maxTimestampDifference int64 = 60
-	if now - timestampInt > maxTimestampDifference {
-		return errors.New("request is more than " + strconv.FormatInt(maxTimestampDifference, 10) + " seconds in the past")
+	if now-timestampInt > maxTimestampDifference {
+		return nil, errors.New("request is more than " + strconv.FormatInt(maxTimestampDifference, 10) + " seconds in the past")
 	}
 
 	signatureBasestring := "v0:" + timestamp + ":" + string(requestBody)
 
 	h := hmac.New(sha256.New, []byte(SlackSigningSecret))
 	h.Write([]byte(signatureBasestring))
-	hexDigest := "v0" + hex.EncodeToString(h.Sum(nil))
+	hexDigest := "v0=" + hex.EncodeToString(h.Sum(nil))
 
 	slackSignature := r.Header.Get("X-Slack-Signature")
+	fmt.Println(hexDigest, slackSignature)
 	if !hmac.Equal([]byte(hexDigest), []byte(slackSignature)) {
-		return errors.New("computed signature and slack signature are not the same; possible malicious slack event")
+		return nil, errors.New("computed signature and slack signature are not the same; possible malicious slack event")
 	}
 
-	return nil
+	return requestBody, nil
 }
 
 func handleUrlVerification(w http.ResponseWriter, r *http.Request, requestBody map[string]interface{}) error {
-	err := verifyRequest(r)
-	if err != nil {
-		return err
-	}
-
 	challenge := requestBody["challenge"].(string)
-	_, err = w.Write([]byte(challenge))
+	_, err := w.Write([]byte(challenge))
 	return err
 }
 
 func SlackEventsListenAndServe() {
 	http.HandleFunc("/slack/action-event", handler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":29138", nil))
 }
